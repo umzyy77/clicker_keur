1133bade1b5ca384cbcaa796012eb8c6377e9ec0
umar <umarnlg@gmail.com>
2025-03-03 00:14:45 +0100

    updated player visu

--- lib/views/game_view.dart
+++ lib/views/game_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../viewmodels/enemy_viewmodel.dart';
 import '../widgets/enemy_widget.dart';
+import '../widgets/player_widget.dart';
 
 class GameView extends StatelessWidget {
   @override
@@ -15,6 +16,8 @@ class GameView extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const EnemyWidget(),
+              const SizedBox(height: 30),
+              const PlayerWidget(playerId: 1), // 🔥 Ajoute l'affichage du joueur
             ],
           ),
         ),
