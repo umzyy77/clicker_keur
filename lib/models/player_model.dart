d1042e24f3ca231e62ebb85ef073ecd3effb2439
Niouzty <epembelefuala@iut.univ-paris8.fr>
2025-03-04 11:42:50 +0100

    ajout des getter de Player

--- lib/models/player_model.dart
+++ lib/models/player_model.dart
@@ -13,6 +13,22 @@ class PlayerModel {
     required this.currentEnemyLevel,
   });
 
+  String getPseudo(){
+    return pseudo;
+  }
+
+  int getTotalExeperience(){
+    return totalExperience;
+  }
+
+  int getDamage(){
+    return clickDamage;
+  }
+
+  int getEnnemyLevel(){
+    return currentEnemyLevel;
+  }
+
   // 🔹 Convertir JSON en PlayerModel
   factory PlayerModel.fromJson(Map<String, dynamic> json) {
     return PlayerModel(
